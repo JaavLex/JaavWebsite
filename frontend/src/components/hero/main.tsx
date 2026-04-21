8e52b166c73b5ab8e24a3035b8b98a67369efa72
Boris <alexandre.javet@hotmail.com>
2026-04-21 08:46:37 +0200

    feat: restructure Hero component layout and improve responsiveness

--- frontend/src/components/hero/main.tsx
+++ frontend/src/components/hero/main.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from 'react-responsive';
 import type { HeroProps } from '../../types/herotypes';
 import HeroPicture from './heropicture';
 import { BiSolidBadgeCheck } from 'react-icons/bi';
@@ -12,49 +13,57 @@ export default function Hero({
 	openToOpportunities,
 	buttons,
 }: HeroProps) {
+	const isMd = useMediaQuery({ query: '(min-width: 768px)' });
+
 	return (
-		<div className="flex flex-col md:flex-row items-center justify-center gap-2 md:gap-16 w-full h-screen">
-			{picture && <HeroPicture src={picture} alt={title} />}
-			<div className="flex flex-col items-center md:items-start gap-4 max-w-4xl">
-				<p className="text-[var(--fg-faint)]">
-					{fakepath &&
-						fakepath.split(' ').map((part, i) =>
-							part.includes('~') ? (
-								<span key={i} className="text-[var(--accent)]">
-									{part + ' '}
-								</span>
-							) : (
-								part + ' '
-							),
-						)}
-				</p>
-				<p className="text-[var(--fg-faint)] italic">
-					{fakecomment && `// ${fakecomment}`}
-				</p>
-				<h1 className="text-9xl font-bold text-center md:text-left typewriter-title whitespace-normal md:whitespace-nowrap">
-					{title}
-				</h1>
-				<p className="text-[var(--fg-faint)]">
-					{fakeconst &&
-						fakeconst.split(/("(?:\\.|[^"\\])*")/g).map((part, i) =>
-							/^"(?:\\.|[^"\\])*"$/.test(part) ? (
-								<span key={i} className="text-[var(--str)]">
-									{part}
-								</span>
-							) : (
-								part
-							),
-						)}
-				</p>
-				<p className="text-lg text-center md:text-left">{subtitle}</p>
-				{openToOpportunities && (
+		<div className="flex flex-col w-full h-screen">
+			<div className="flex items-center md:items-end justify-center md:justify-end pt-14 md:pt-24 md:pr-24 w-full ">
+				{openToOpportunities && isMd && (
 					<pre className="flex items-center gap-2">
 						<BiSolidBadgeCheck className="glowing-text-badge" size={24} />
-						<p className="text-[var(--fg-dim)] text-2xl">
+						<p className="text-[var(--fg-dim)] md:text-2xl">
 							Open to job opportunities!
 						</p>
 					</pre>
 				)}
+			</div>
+			<div className="flex flex-col md:flex-row items-center justify-center gap-2 md:gap-16 w-full h-screen">
+				{picture && <HeroPicture src={picture} alt={title} />}
+				<div className="flex flex-col items-center md:items-start gap-4 max-w-4xl">
+					<p className="text-[var(--fg-faint)]">
+						{fakepath &&
+							fakepath.split(' ').map((part, i) =>
+								part.includes('~') ? (
+									<span key={i} className="text-[var(--accent)]">
+										{part + ' '}
+									</span>
+								) : (
+									part + ' '
+								),
+							)}
+					</p>
+					<p className="text-[var(--fg-faint)] italic">
+						{fakecomment && `// ${fakecomment}`}
+					</p>
+					<h1 className="text-9xl font-bold text-center md:text-left typewriter-title whitespace-normal md:whitespace-nowrap">
+						{title}
+					</h1>
+					<p className="text-[var(--fg-faint)]">
+						{fakeconst &&
+							fakeconst.split(/("(?:\\.|[^"\\])*")/g).map((part, i) =>
+								/^"(?:\\.|[^"\\])*"$/.test(part) ? (
+									<span key={i} className="text-[var(--str)]">
+										{part}
+									</span>
+								) : (
+									part
+								),
+							)}
+					</p>
+					<p className="text-lg text-justify px-10 md:text-left md:px-0">
+						{subtitle}
+					</p>
+				</div>
 			</div>
 		</div>
 	);
